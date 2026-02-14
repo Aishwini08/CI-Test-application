a51a9ba9f8dde326bf5a62ddaacadee0d189db86
Aishwini08 <aishwinipriyapillai@gmail.com>
2026-02-14 19:08:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-const username = process.env.USER_NAME;
-const password = process.env.PASSWORD;
-
-if (!username || !password) {
-  throw new Error("Missing credentials");
-}
-
-console.log("Credentials received successfully");
-console.log("Username:", username);
-// Never log passwords in real projects
-
-
-
+console.log("Hello World");
